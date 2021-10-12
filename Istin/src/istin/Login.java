efd49d3ea8ed7c4ce4c3a99d04214f20cf110ef9
davipatury <davi.patury@yahoo.com.br>
2021-10-12 15:39:19 -0300

    nova leitura do json

--- Istin/src/istin/Login.java
+++ Istin/src/istin/Login.java
@@ -1,109 +1,67 @@
 package istin;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
+import org.json.JSONArray;
+import org.json.JSONTokener;
 
 
 public class Login {
+    private JSONArray logins;
+    
+    public Login() {
+        try {
+            FileInputStream is = new FileInputStream(new File("logins.json"));
+            JSONTokener tokener = new JSONTokener(is);
+            logins = new JSONArray(tokener);
+        } catch(FileNotFoundException e) {
+            logins = new JSONArray();
+        }
+    }
+    
+    public void salvarJson() {
+        try {
+            FileWriter escreve = new FileWriter("logins.json");
+            escreve.write(logins.toString(4));
+            escreve.close();
+        } catch(IOException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     public void criaNovaConta(Cliente cliente) {
-
-            JSONArray json;
-            JSONParser jsonParser = new JSONParser();
-
-            FileWriter escreve = null;
-
-            try {
-                HashMap informacoesCliente = new HashMap();
-                informacoesCliente.put("nome", cliente.getNome());
-                informacoesCliente.put("email", cliente.getEmail());
-                informacoesCliente.put("senha", cliente.getSenha());
-                informacoesCliente.put("saldo", cliente.getSaldo());
-
-                json = (JSONArray) jsonParser.parse(new FileReader("logins.json"));
-                cliente.setId(json.size());
-                json.add(informacoesCliente);
-
-                System.out.println(json.toJSONString());
-
-                escreve = new FileWriter("logins.json");
-                escreve.write(json.toJSONString());
-                escreve.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException ex) {
-                java.util.logging.Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ParseException ex) {
-                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-
+        HashMap informacoesCliente = new HashMap();
+        informacoesCliente.put("nome", cliente.getNome());
+        informacoesCliente.put("email", cliente.getEmail());
+        informacoesCliente.put("senha", cliente.getSenha());
+        informacoesCliente.put("saldo", cliente.getSaldo());
+        
+        cliente.setId(logins.length());
+        logins.put(informacoesCliente);
+        
+        this.salvarJson();
     }
 
     public void criaNovaConta(Autor autor) {
-        JSONArray json;
-        JSONParser jsonParser = new JSONParser();
-
-        FileWriter escreve = null;
-
-        try {
-            HashMap informacoesCliente = new HashMap();
-            informacoesCliente.put("nome", autor.getNome());
-            informacoesCliente.put("email", autor.getEmail());
-            informacoesCliente.put("senha", autor.getSenha());
-
-            json = (JSONArray) jsonParser.parse(new FileReader("logins.json"));
-            autor.setId(json.size()+100);
-            json.add(informacoesCliente);
-
-            System.out.println(json.toJSONString());
-
-            escreve = new FileWriter("logins.json");
-            escreve.write(json.toJSONString());
-            escreve.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParseException ex) {
-            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+        HashMap informacoesAutor = new HashMap();
+        informacoesAutor.put("nome", autor.getNome());
+        informacoesAutor.put("email", autor.getEmail());
+        informacoesAutor.put("senha", autor.getSenha());
+        
+        autor.setId(logins.length() + 100);
+        logins.put(informacoesAutor);
+        
+        this.salvarJson();
     }
-
+    
     public boolean validaLogin(String nomeInserido, String senhaInserida) {
-
-        JSONArray json;
-        JSONParser jsonParser = new JSONParser();
-
-        try {
-            json = (JSONArray) jsonParser.parse(new FileReader("logins.json"));
-            System.out.println(json.toJSONString());
-            
-            for (var objeto : json){
-                JSONObject usuario = (JSONObject) objeto;
-                if(usuario.get("nome").equals(nomeInserido)){
-                    if(usuario.get("senha").equals(senhaInserida)) return true;
-                }
-            }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParseException ex) {
-            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return false;
+        return logins.toList().stream().map(o -> (HashMap) o).anyMatch(o -> o.get("nome").equals(nomeInserido) && o.get("senha").equals(senhaInserida));
     }
 }
