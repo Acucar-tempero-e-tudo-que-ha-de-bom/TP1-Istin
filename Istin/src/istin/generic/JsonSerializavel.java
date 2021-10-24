3e878d9148e6be1bd2cc07601ca1c2b919399a3e
davipatury <davi.patury@yahoo.com.br>
2021-10-24 01:51:33 -0300

    json serializavel comments

--- Istin/src/istin/generic/JsonSerializavel.java
+++ Istin/src/istin/generic/JsonSerializavel.java
@@ -4,9 +4,21 @@ import org.json.JSONObject;
 
 public interface JsonSerializavel {
     
+    /**
+     * @return id desse objeto 
+     */
     public Integer getId();
+    
+    /**
+     * Define o id desse objeto.
+     * @param id novo id do objeto
+     */
     public void setId(Integer id);
     
+    /**
+     * Transforma esse objeto em um objeto json.
+     * @return objeto json que representa esse objeto
+     */
     public JSONObject toJSON();
     
 }
